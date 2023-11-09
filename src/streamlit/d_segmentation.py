58570ec1deb9629cc813a37fcd0bb6681ee88d76
Iréné <amieheessomba@etu.unistra.fr>
2023-11-09 15:45:11 +0100

    d_segmentation

--- src/streamlit/d_segmentation.py
+++ src/streamlit/d_segmentation.py
@@ -13,9 +13,9 @@ def content() :
    path1 = "./reports/figures/test-classifpatient/classif-256-0.4_sample_graphs_segmented_gradcam_guidedGradcam.png"
    path2 = "./reports/figures/test-classifpatient/classif-256-0.4_sample_graphs_gradcam_guidedGradcam.png"
 
-   with st.expander("Images non-segementatees"):
+   with st.expander("Images non-segmentatées"):
       im1 = plt.imread(path2)
       st.image(im1)
-   with st.expander("Images Segementées"):
+   with st.expander("Images Segmentées"):
        im2 = plt.imread(path1)
        st.image(im2)
