0cecb6d4916a10efb003adcecdc69a34520a2be8
Iréné <amieheessomba@etu.unistra.fr>
2023-11-09 15:27:38 +0100

    up

--- src/streamlit/d_segmentation.py
+++ src/streamlit/d_segmentation.py
@@ -1,9 +1,22 @@
-import streamlit as st
 from src.streamlit.mods.histogram_color import compute
 
-
 def content() :
-    st.header("Interprétabilité et Segmentation")
-    # st.markdown("")
-    st.subheader('Segmentation sémantique des images')
-    compute(st=st)
+   import streamlit as st
+   import matplotlib.pyplot as plt 
+
+   st.header("Segmentation et Interprétabilité")
+   # st.markdown("")
+   st.subheader('Segmentation sémantique des images')
+   compute(st=st)
+   st.subheader('Interprétabilité')
+
+   path1 = "./reports/figures/test-classifpatient/classif-256-0.4_sample_graphs_segmented_gradcam_guidedGradcam.png"
+   path2 = "./reports/figures/test-classifpatient/classif-256-0.4_sample_graphs_gradcam_guidedGradcam.png"
+
+   
+   with st.expander("No segementation"):
+      im1 = plt.imread(path2)
+      st.image(im1)
+   with st.expander("Segementation"):
+       im2 = plt.imread(path1)
+       st.image(im2)
